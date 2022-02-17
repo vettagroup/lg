0d6f819b656a202029f8b1303e72df7bd724a213
Matthew McTague <matthew.mctague@vetta.nz>
2022-02-17 16:33:44 +1300

    aspath test 13

--- htdocs/index.php
+++ htdocs/index.php
@@ -1058,7 +1058,6 @@ function parse_out($output, $check = FALSE)
 			$aspath = $matches[1];
 			preg_match("/((?:\d+)+)/", $aspath, $matches);
 			$matchCount = 0;
-			die(var_dump($aspath));
 			foreach($matches as $m){
 				if($matchCount == 0){
 					continue;
