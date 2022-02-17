8d24582dbf5fbaed73c14d4b94d6e8fc9e579c19
Matthew McTague <matthew.mctague@vetta.nz>
2022-02-17 16:52:35 +1300

    aspath test 26

--- htdocs/index.php
+++ htdocs/index.php
@@ -1070,7 +1070,6 @@ function parse_out($output, $check = FALSE)
 					if(!empty($summary_part_asmatches)){
 						foreach($summary_part_asmatches as $m){
 							$summary_part = stripslashes(str_replace(array_keys($summary_part_asmatches), array_values($summary_part_asmatches), $summary_part));
-							die(var_dump($summary_part));
 						}
 					}
 				}
