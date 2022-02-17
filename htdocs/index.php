8dc5a92be487107de33c976deded63b6f7efa44f
Matthew McTague <matthew.mctague@vetta.nz>
2022-02-17 14:38:02 +1300

    test1

--- htdocs/index.php
+++ htdocs/index.php
@@ -1049,7 +1049,7 @@ function parse_out($output, $check = FALSE)
 			$data_exp = explode(' ', trim($summary_part), 3);
 
 			if(!$ipsafe){
-				$summary_part = preg_replace("/\svia\s\s?\S+/x", "", $summary_part);
+				#$summary_part = preg_replace("/\svia\s\s?\S+/x", "", $summary_part);
 			}
 
 			$summary_part = preg_replace_callback(
